32b78a4da16527fbcd57c97105a4390cd281f203
Alexander Andrianov <tracktouch@gmail.com>
2021-06-09 00:01:33 +0300

    Search flow MVP

--- iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
+++ iOSArchitecturesDemo/Model/Services/AppStartManager/AppStartManager.swift
@@ -9,30 +9,30 @@
 import UIKit
 
 final class AppStartManager {
+  
+  var window: UIWindow?
+  
+  init(window: UIWindow?) {
+    self.window = window
+  }
+  
+  func start() {
+    let rootVC = SearchModuleBuilder.build()
+    rootVC.navigationItem.title = "Search via iTunes"
     
-    var window: UIWindow?
+    let navVC = self.configuredNavigationController
+    navVC.viewControllers = [rootVC]
     
-    init(window: UIWindow?) {
-        self.window = window
-    }
-    
-    func start() {
-        let rootVC = SearchViewController()
-        rootVC.navigationItem.title = "Search via iTunes"
-        
-        let navVC = self.configuredNavigationController
-        navVC.viewControllers = [rootVC]
-        
-        window?.rootViewController = navVC
-        window?.makeKeyAndVisible()
-    }
-    
-    private lazy var configuredNavigationController: UINavigationController = {
-        let navVC = UINavigationController()
-        navVC.navigationBar.barTintColor = UIColor.varna
-        navVC.navigationBar.isTranslucent = false
-        navVC.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        navVC.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
-        return navVC
-    }()
+    window?.rootViewController = navVC
+    window?.makeKeyAndVisible()
+  }
+  
+  private lazy var configuredNavigationController: UINavigationController = {
+    let navVC = UINavigationController()
+    navVC.navigationBar.barTintColor = UIColor.varna
+    navVC.navigationBar.isTranslucent = false
+    navVC.navigationBar.largeTitleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    navVC.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+    return navVC
+  }()
 }
