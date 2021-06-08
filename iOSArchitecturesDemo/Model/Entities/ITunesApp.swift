d4c9d74d6f4170b08b439ccbd99fec7fb6b8fb20
Alexander Andrianov <tracktouch@gmail.com>
2021-06-09 01:38:44 +0300

    ITunesApp model extended

--- iOSArchitecturesDemo/Model/Entities/ITunesApp.swift
+++ iOSArchitecturesDemo/Model/Entities/ITunesApp.swift
@@ -22,6 +22,9 @@ public struct ITunesApp: Codable {
     public let size: Bytes?
     public let iconUrl: String?
     public let screenshotUrls: [String]
+    public let version: String
+    public let currentVersionReleaseDate: String
+    public let releaseNotes: String?
     
     // MARK: - Codable
     
@@ -36,6 +39,9 @@ public struct ITunesApp: Codable {
         case size = "fileSizeBytes"
         case iconUrl = "artworkUrl512"
         case screenshotUrls = "screenshotUrls"
+        case version = "version"
+        case currentVersionReleaseDate = "currentVersionReleaseDate"
+        case releaseNotes = "releaseNotes"
     }
     
     public init(from decoder: Decoder) throws {
@@ -50,6 +56,10 @@ public struct ITunesApp: Codable {
         self.size = (try? container.decode(String.self, forKey: .size)) >>- { Bytes($0) }
         self.iconUrl = try? container.decode(String.self, forKey: .iconUrl)
         self.screenshotUrls = (try? container.decode([String].self, forKey: .screenshotUrls)) ?? []
+        self.version = try container.decode(String.self, forKey: .version)
+        self.currentVersionReleaseDate = try container
+          .decode(String.self, forKey: .currentVersionReleaseDate)
+        self.releaseNotes = try? container.decode(String.self, forKey: .releaseNotes)
     }
     
     // MARK: - Init
@@ -63,7 +73,10 @@ public struct ITunesApp: Codable {
                   averageRatingForCurrentVersion: Float?,
                   size: Bytes?,
                   iconUrl: String?,
-                  screenshotUrls: [String]) {
+                  screenshotUrls: [String],
+                  version: String,
+                  currentVersionReleaseDate: String,
+                  releaseNotes: String?) {
         self.appName = appName
         self.appUrl = appUrl
         self.company = company
@@ -74,5 +87,8 @@ public struct ITunesApp: Codable {
         self.size = size
         self.iconUrl = iconUrl
         self.screenshotUrls = screenshotUrls
+        self.version = version
+        self.currentVersionReleaseDate = currentVersionReleaseDate
+        self.releaseNotes = releaseNotes
     }
 }
