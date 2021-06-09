cbffc5bcae2030a26401f16f95edfe8f840e83d0
Alexander Andrianov <tracktouch@gmail.com>
2021-06-10 02:09:53 +0300

    reorganized AppDetailVC

--- iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailViewController.swift
+++ iOSArchitecturesDemo/Flows/AppDetail/Controller/AppDetailViewController.swift
@@ -10,11 +10,15 @@ import UIKit
 
 final class AppDetailViewController: UIViewController {
   
-  let app: ITunesApp
-  lazy var factory = AppDetailModelFactory(app: app)
+  private let app: ITunesApp
+  private lazy var factory = AppDetailModelFactory(app: app)
   
-  lazy var headerViewController = AppDetailHeaderViewController(model: factory.headerModel())
-  lazy var releaseInfoViewController = AppDetailReleaseInfoViewController(model: factory.releaseInfoModel())
+  private lazy var headerViewController = AppDetailHeaderViewController(
+    model: factory.headerModel())
+  private lazy var screenshotsViewController = AppDetailScreenshotsViewController(
+    model: factory.screenshotModel())
+  private lazy var releaseInfoViewController = AppDetailReleaseInfoViewController(
+    model: factory.releaseInfoModel())
   
   init(app: ITunesApp) {
     self.app = app
@@ -30,12 +34,20 @@ final class AppDetailViewController: UIViewController {
     self.configureUI()
   }
   
+  @objc func didTapOnScreenshot(sender: Any?) {
+    let indexToPresent = screenshotsViewController.selectedIndex
+    let fullScreenVC = AppDetailFullScreenshotsViewController(
+      model: factory.screenshotModel(), indexToStart: indexToPresent)
+    present(fullScreenVC, animated: true)
+  }
+  
   private func configureUI() {
     self.view.backgroundColor = .white
     self.navigationController?.navigationBar.tintColor = UIColor.white
     self.navigationItem.largeTitleDisplayMode = .never
     self.addHeaderViewController()
-    self.addDescriptionViewController()
+    self.addScreenshotsViewController()
+    self.addReleaseInfoViewController()
   }
   
   private func addHeaderViewController() {
@@ -51,17 +63,30 @@ final class AppDetailViewController: UIViewController {
     ])
   }
   
-  private func addDescriptionViewController() {
+  private func addScreenshotsViewController() {
+    self.addChild(self.screenshotsViewController)
+    self.view.addSubview(self.screenshotsViewController.view)
+    self.screenshotsViewController.didMove(toParent: self)
+    
+    self.screenshotsViewController.view.translatesAutoresizingMaskIntoConstraints = false
+    NSLayoutConstraint.activate([
+      self.screenshotsViewController.view.topAnchor.constraint(equalTo: headerViewController.view.bottomAnchor),
+      self.screenshotsViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
+      self.screenshotsViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor),
+      self.screenshotsViewController.view.heightAnchor.constraint(equalToConstant: 230)
+    ])
+  }
+  
+  private func addReleaseInfoViewController() {
     self.addChild(releaseInfoViewController)
     self.view.addSubview(releaseInfoViewController.view)
     releaseInfoViewController.didMove(toParent: self)
     
     releaseInfoViewController.view.translatesAutoresizingMaskIntoConstraints = false
     NSLayoutConstraint.activate([
-      releaseInfoViewController.view.topAnchor.constraint(equalTo: self.headerViewController.view.bottomAnchor),
+      releaseInfoViewController.view.topAnchor.constraint(equalTo: self.screenshotsViewController.view.bottomAnchor),
       releaseInfoViewController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor),
       releaseInfoViewController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor)
-//      releaseInfoViewController.view.heightAnchor.constraint(equalToConstant: 250.0)
     ])
   }
 }
