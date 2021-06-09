77b743f44ee38eb65f2b6abea2519b6acea0c7bc
Alexander Andrianov <tracktouch@gmail.com>
2021-06-09 15:31:04 +0300

    added AppDetailModel factory + models

---
+++ iOSArchitecturesDemo/Flows/AppDetail/View/AppDetailReleaseInfoView.swift
@@ -0,0 +1,9 @@
+//
+//  AppDetailReleaseInfoView.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Alexander Andrianov on 09.06.2021.
+//  Copyright © 2021 ekireev. All rights reserved.
+//
+
+import Foundation
