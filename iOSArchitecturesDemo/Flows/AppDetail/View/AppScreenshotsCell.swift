585fcb7a18edea20d8e6d716233b44bc9bb3126b
Alexander Andrianov <tracktouch@gmail.com>
2021-06-10 02:07:35 +0300

    Added screenshots collection cell

--- iOSArchitecturesDemo/Flows/AppDetail/View/AppScreenshotsCell.swift
+++ iOSArchitecturesDemo/Flows/AppDetail/View/AppScreenshotsCell.swift
@@ -6,4 +6,42 @@
 //  Copyright © 2021 ekireev. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+final class AppScreenshotsCell: UICollectionViewCell {
+  override var reuseIdentifier: String? {
+    return "AppScreenshotsCell"
+  }
+  
+  private let screenshotImageView: UIImageView = {
+    let imageView = UIImageView()
+    imageView.contentMode = .scaleAspectFill
+    imageView.clipsToBounds = true
+    return imageView
+  }()
+  
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+    addScreenshotImageView()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  func setImage(image: UIImage) {
+    screenshotImageView.image = image
+  }
+  
+  private func addScreenshotImageView() {
+    addSubview(screenshotImageView)
+    screenshotImageView.translatesAutoresizingMaskIntoConstraints = false
+    
+    NSLayoutConstraint.activate([
+      screenshotImageView.topAnchor.constraint(equalTo: topAnchor),
+      screenshotImageView.bottomAnchor.constraint(equalTo: bottomAnchor),
+      screenshotImageView.leadingAnchor.constraint(equalTo: leadingAnchor),
+      screenshotImageView.trailingAnchor.constraint(equalTo: trailingAnchor)
+    ])
+  }
+}
