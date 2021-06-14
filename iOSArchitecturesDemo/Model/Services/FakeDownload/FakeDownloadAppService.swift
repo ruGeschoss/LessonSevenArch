7a6d56f2ae38fa0350d085ab692dd0025ad4a1fa
Alexander Andrianov <tracktouch@gmail.com>
2021-06-14 18:41:29 +0300

    added Observable

---
+++ iOSArchitecturesDemo/Model/Services/FakeDownload/FakeDownloadAppService.swift
@@ -0,0 +1,9 @@
+//
+//  FakeDownloadAppService.swift
+//  iOSArchitecturesDemo
+//
+//  Created by Alexander Andrianov on 14.06.2021.
+//  Copyright © 2021 ekireev. All rights reserved.
+//
+
+import Foundation
