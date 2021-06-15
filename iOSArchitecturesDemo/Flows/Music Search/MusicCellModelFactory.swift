f3f6b9078c503f646cd6264e994c0c90313e0873
Alexander Andrianov <tracktouch@gmail.com>
2021-06-16 00:19:04 +0300

    updated MusicCellModel factory (no need in DateFormatter)

--- iOSArchitecturesDemo/Flows/Music Search/MusicCellModelFactory.swift
+++ iOSArchitecturesDemo/Flows/Music Search/MusicCellModelFactory.swift
@@ -18,36 +18,24 @@ struct MusicCellModel {
 
 final class MusicCellModelFactory {
   
-  private lazy var formatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateFormat = "mm:ss"
-    return formatter
-  }()
-  
-  private var timeCache: [Int: String] = [:]
-  
   func createMusicCellModel(from model: ITunesSong) -> MusicCellModel {
-    let trackTime = getTrackTime(millisec: model.trackTimeMillis)
-    
+    let trackTime = convertTrackTimeToString(timeInMillis: model.trackTimeMillis)
+
     return MusicCellModel(trackName: model.trackName,
                           collectionName: model.collectionName,
                           artistName: model.artistName,
                           artwork: model.artwork,
                           trackTime: trackTime)
   }
-  
-  private func getTrackTime(millisec: Int?) -> String? {
-    guard let time = millisec else { return nil }
-    
-    if let storedTime = timeCache[time] {
-      return storedTime
-    } else {
-      let timeInSeconds = Double(time) / 1000
-      let dateFromSecond = Date(timeIntervalSince1970: timeInSeconds)
-      let trackTime = formatter.string(from: dateFromSecond)
-      timeCache[time] = trackTime
-      return trackTime
-    }
+
+  private func convertTrackTimeToString(timeInMillis: Int?) -> String? {
+    guard let timeInMillis = timeInMillis else { return nil }
+    let timeInSec = timeInMillis / 1000
+    let minutes = timeInSec / 60
+    let seconds = timeInSec % 60
+    let stringMinutes = minutes < 10 ? "0\(minutes)" : "\(minutes)"
+    let stringSeconds = seconds < 10 ? "0\(seconds)" : "\(seconds)"
+    return stringMinutes + ":" + stringSeconds
   }
   
 }
